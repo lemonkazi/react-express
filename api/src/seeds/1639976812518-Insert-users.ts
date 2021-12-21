3b674eb26d2599060017e3fb44cb8b4db4197c20
lemonkazi <lemonpstu09@gmail.com>
2021-12-21 08:38:21 +0600

    table name error solved on seed

--- api/src/seeds/1639976812518-Insert-users.ts
+++ api/src/seeds/1639976812518-Insert-users.ts
@@ -4,7 +4,7 @@ export class InsertUsers1639976812518 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            INSERT into USERS(username, email, password ,role) VALUES
+            INSERT into users(username, email, password ,role) VALUES
             ('sadmin', 'sadmin@test.dev', 'sadmin', 'superAdmin'),
             ('tadmin', 'tadmin@test.dev', 'tadmin','admin'),
             ('user', 'user@test.dev', 'user','user');
@@ -13,7 +13,7 @@ export class InsertUsers1639976812518 implements MigrationInterface {
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DELETE FROM USERS CASCADE;
+            DELETE FROM users CASCADE;
         `);
     }
 
